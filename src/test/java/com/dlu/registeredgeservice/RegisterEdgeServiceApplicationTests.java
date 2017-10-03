ba2aa3b1480e57b5c151058b4cf53456043498ea
DayongLu <ludayong20@gmail.com>
2017-10-02 23:11:05 -0400

    initial

---
+++ src/test/java/com/dlu/registeredgeservice/RegisterEdgeServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dlu.registeredgeservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RegisterEdgeServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
